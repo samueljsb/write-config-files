4e34d7f8930079978af50f3e745b667c2cc27035
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2023-08-30 01:54:33 +0100

    Remove duplication from dry run file writer

--- write_config_files/files.py
+++ write_config_files/files.py
@@ -33,17 +33,6 @@ class FileSystem:
             os.chmod(path, 0o744)
 
 
-@attrs.frozen
-class DryRunFileWriter:
-    def file_exists(self, path: str) -> bool:
-        return os.path.exists(path)
-
-    def read_content(self, path: str) -> str:
-        try:
-            with open(path) as f:
-                return f.read()
-        except FileNotFoundError:
-            return ''
-
+class DryRunFileWriter(FileSystem):
     def write_file(self, path: str, content: str, is_executable: bool) -> None:
         pass
